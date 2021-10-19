879222495c46751f923d73f7b5c9ebfb23309112
Hassan Ali <prog.hasan@gmail.com>
2021-10-19 11:09:57 +0300

    First commit

---
+++ install/language/en-gb/common/footer.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_project']       = 'Project Homepage';
+$_['text_project_arab']  = 'موقع أوبن كارت العرب';
+$_['text_documentation'] = 'Documentation';
+$_['text_support']       = 'Support Forums';
